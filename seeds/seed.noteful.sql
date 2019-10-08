57c0086d641618f268d376e84b175a3a4742e000
Yulia Khisamutdinova <yulia-kh@users.noreply.github.com>
2019-10-08 14:59:42 -0700

    add routes for folders and WIP routes for notes

---
+++ seeds/seed.noteful.sql
@@ -0,0 +1,11 @@
+INSERT INTO folders (folder_name)
+VALUES
+  ('Thinkful'),
+  ('Google'),
+  ('MDN');
+
+  INSERT INTO notes (title, content, folder_id)
+VALUES
+  ('Thinkful notes', 'Think outside the classroom', 1),
+  ('Google notes', 'Where we find everything else', 2),
+  ('MDN notes', 'The only place to find web documentation', 3);
--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
+const foldersRouter = require('./folders/folders-router');
+const notesRouter = require('./notes/notes-router');
+
 
 const app = express();
 
@@ -15,9 +18,8 @@ app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!')
-});
+app.use('/api/folders', foldersRouter);
+app.use('/api/notes', notesRouter);
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
